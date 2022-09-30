14abaadb15e6a28cef8927967b5bdb58468d4e78
binginsin <binginsin@gmail.com>
2022-09-30 22:45:40 +0100

    Added unit test to conver RicoSuter/NSwag#4138

--- src/NSwag.CodeGeneration.CSharp.Tests/InheritanceTests.cs
+++ src/NSwag.CodeGeneration.CSharp.Tests/InheritanceTests.cs
@@ -42,5 +42,47 @@ namespace NSwag.CodeGeneration.TypeScript.Tests
             // Assert
             Assert.Contains("class SessionStateResent : SportsbookEventBody", code);
         }
+
+        [Fact]
+        public async Task When_generating_csharp_with_any_inheritance_and_additional_properties_and_no_own_properties_then_inheritance_is_generated()
+        {
+
+            var json = @"{
+""openapi"": ""3.0.0"",
+""components"": {
+""schemas"": {
+    ""SessionStateResent"": {
+        ""type"": ""object"",
+        ""allOf"": [
+            {
+                ""$ref"": ""#/components/schemas/SportsbookEventBody""
+            }
+        ],
+        ""additionalProperties"": {}
+    },
+    ""SportsbookEventBody"": {
+        ""type"": ""object"",
+        ""properties"": {
+              ""minVer"": {
+                ""type"": ""integer"",
+                ""format"": ""int32""
+              }
+        },
+        ""additionalProperties"": {}
+    }
+}
+}
+}";
+
+            var document = await OpenApiDocument.FromJsonAsync(json);
+
+            // Act
+            var settings = new CSharpClientGeneratorSettings();
+            var generator = new CSharpClientGenerator(document, settings);
+            var code = generator.GenerateFile();
+
+            // Assert
+            Assert.Contains("class SessionStateResent : SportsbookEventBody", code);
+        }
     }
 }
